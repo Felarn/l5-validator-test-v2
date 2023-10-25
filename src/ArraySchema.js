f0a255764c020b3d5fa891902273c1e63f07afe0
Александр Шмелев <120136549+Felarn@users.noreply.github.com>
2023-10-26 00:54:39 +0300

    it works, kinda

--- src/ArraySchema.js
+++ src/ArraySchema.js
@@ -1,3 +1,21 @@
 export default class ArraySchema {
-  constructor() {}
+  constructor() {
+    this.validators = [(value) => Array.isArray(value)];
+  }
+
+  isValid(value) {
+    return this.validators.every((validator) => validator(value));
+  }
+
+  maxDepth(depth) {
+    const depthCheck = (value, depthAllowed) => {
+      console.log(value, depthAllowed);
+      if (!Array.isArray(value)) return true;
+      if (depthAllowed < 0) return false;
+      return value.every((elem) => depthCheck(elem, depthAllowed - 1));
+    };
+
+    this.validators.push((arrayToCheck) => depthCheck(arrayToCheck, depth));
+    return this;
+  }
 }
--- src/ObjectSchema.js
+++ src/ObjectSchema.js
@@ -1,3 +1,26 @@
 export default class ObjectSchema {
-  constructor() {}
+  constructor() {
+    this.template = [];
+    this.validators = [
+      (objToCheck) =>
+        Object.keys(objToCheck).length === Object.keys(this.template).length,
+    ];
+  }
+
+  shape(template) {
+    this.template = template;
+    this.validators.push((objToCheck) =>
+      Object.keys(this.template).every((key) =>
+        this.template[key].isValid(objToCheck[key])
+      )
+    );
+    return this;
+  }
+
+  isValid(objToCheck) {
+    console.log("##==================>", objToCheck);
+    const result = this.validators.every((validator) => validator(objToCheck));
+    console.log("------------------------>", result);
+    return result;
+  }
 }
